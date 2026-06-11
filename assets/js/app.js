03f6917356a9603c9de90f1a3244f2fb31f0d04a
Martinolotto <martinotiktok1@gmail.com>
2026-06-11 16:08:51 -0300

    feat:se agregó la funcón para cargar las tarjetas en el dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,11 +16,42 @@ async function obtenerPersonajes() {
         personajesGlobal= datosJSON.docs; 
 
         console.log("Personajes cargados:", personajesGlobal);
-        
 
+        renderizarTarjetas(personajesGlobal);
+        
     } catch (error) {
         
         console.error("Hubo un problema al conectar con la API:", error);
     }
 }
 obtenerPersonajes();
+
+// cargar personajes en el html
+function renderizarTarjetas(arregloPersonajes) {
+    contenedorPersonajes.innerHTML = '';
+    if (arregloPersonajes.length === 0) {
+        return;
+    }
+    arregloPersonajes.forEach(personaje => {
+       
+        const urlImagenCompleta = `https://cdn.thesimpsonsapi.com/500${personaje.image}`;
+
+        //estructura de la tarjeta
+        const tarjetaHTML = `
+            <div class="col">
+                <div class="card h-100 shadow-sm border-warning">
+                    <img src="${urlImagenCompleta}" class="card-img-top p-3" alt="${personaje.name}" style="height: 300px; object-fit: contain;">
+                    <div class="card-body d-flex flex-column text-center">
+                        <h5 class="card-title fw-bold">${personaje.name}</h5>
+                        <p class="card-text mb-1"><strong>Ocupación:</strong> ${personaje.occupation}</p>
+                        <p class="card-text mb-3"><strong>Estado:</strong> ${personaje.status}</p>
+                        <button class="btn btn-warning mt-auto btn-detalle" data-id="${personaje._id}">Ver detalle</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // se carga la tarjeta en el contenedor 
+        contenedorPersonajes.innerHTML += tarjetaHTML;
+    });
+}
